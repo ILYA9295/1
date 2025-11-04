7b5789f64e4d6e151bde95f7fb351d036d4a8f03
ILYA9295 <60582705+ILYA9295@users.noreply.github.com>
2025-11-04 16:00:20 +0300

    Delete AntiHate/schema/migrations directory

--- AntiHate/schema/migrations/001_init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-    id SERIAL PRIMARY KEY,
-    chat_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
